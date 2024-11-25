6471eba3e99ae3abbe7e083375d5519de0583e92
7879827369 <norielyntalavera@gmail.com>
2024-11-25 17:40:08 +0800

    Create about.php

---
+++ about.php
@@ -0,0 +1,47 @@
+            </div>
+        </div>
+       
+        <div class="column">
+            <div class="card">
+                <img src="image/Jeric.jfif" alt="Jeric Mendoza" style="width: 160px;">
+                <h3>Jeric Mendoza</h3>
+                <p>Programmer</p>
+            </div>
+        </div>
+
+
+        <div class="column">
+            <div class="card">
+                <img src="image/sarahmel.jfif" alt="Sarahmel Ocado" style="width: 160px;">
+                <h3>Sarahmel Ocado</h3>
+                <p>Web Designer</p>
+            </div>
+        </div>
+       
+        <div class="column">
+            <div class="card">
+                <img src="image/rennalien.jfif"alt="" style="width: 160px;">
+                <h3>Rennalien Oliva</h3>
+                <p>Web Manager</p>
+            </div>
+        </div>
+       
+        <div class="column">
+            <div class="card">
+                <img src="image/McEphraem.jfif" alt="Mc Ephraem SAn Jose" style="width: 160px;">
+                <h3>Mc Ephraem San Jose</h3>
+                <p>Web Designer</p>
+            </div>
+        </div>
+
+
+        <div class="column">
+            <div class="card">
+                <img src="image/Norielyn.jfif" alt="Norielyn Talavera" style="width: 160px;">
+                <h3>Norielyn Talavera</h3>
+                <p>Web Designer</p>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
